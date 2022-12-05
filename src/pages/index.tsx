ea218fbeaac857649e8e9c7fb8ff8e26e5a9ae3c
Kazuki Ogura <84998022+vivalaki45@users.noreply.github.com>
2022-12-05 10:22:29 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ const RenderPage = () => (
     <div>
       <h2>Welcome!</h2>
       <p>おかずが、知識をアウトプットするためのウェブサイトです。</p>
-      <h2>SNS<h2>
+      <h2>SNS</h2>
       <p>
         <ExtLink href="https://twitter.com/vivala_ki">
           Twitter
         </ExtLink>
       </p>
       <p>
-        <ExtLink href="https://www.instagram.com/vivalaki_o">
+        <ExtLink href="https://www.instagram.com/vivalaki_o/?next=%2F">
           Instagram
         </ExtLink>
       </p>
